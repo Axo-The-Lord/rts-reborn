81fa828533ed42d45bb72315deed5c7640599d77
Altzeus <88247068+AltTheZeus@users.noreply.github.com>
2022-01-09 12:29:26 +0300

    Neik and Bezier
    Some NK functions and some Bezier functions, all used in artificer.lua

--- Core/util.lua
+++ Core/util.lua
@@ -56,3 +56,82 @@ function contains(t, value)
 		return false
 	end
 end
+
+-- Neik's functions and stuff 
+function angleDif(current, target)
+  return ((((current - target) % 360) + 540) % 360) - 180
+end
+local syncInputRelease = net.Packet.new("SSInputRelease", function(sender, player, key)
+	local playerI = player:resolve()
+	if playerI and playerI:isValid() and key then
+		playerI:getData()._keyRelease = key
+	end
+end)
+local hostSyncInputRelease = net.Packet.new("SSInputRelease2", function(sender, player, key)
+	local playerI = player:resolve()
+	if playerI and playerI:isValid() and key then
+		playerI:getData()._keyRelease = key
+		syncInputRelease:sendAsHost(net.EXCLUDE, sender, player, key)
+	end
+end)
+
+function syncControlRelease(player, control)
+	if player:control(control) == input.RELEASED then
+		if net.online and net.localPlayer == player then
+			if net.host then
+				syncInputRelease:sendAsHost(net.ALL, nil, player:getNetIdentity(), control)
+			else
+				hostSyncInputRelease:sendAsClient(player:getNetIdentity(), control)
+			end
+		end
+			
+		return true
+		
+	elseif player:getData()._keyRelease == control then
+		player:getData()._keyRelease = nil
+		
+		return true
+	else
+	
+		return false
+	end
+end 
+
+function distance(x1, y1, x2, y2)
+	local distance = math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2))
+	return distance
+end
+function posToAngle(x1, y1, x2, y2, rad)
+	local deltaX = x2 - x1
+	local deltaY = y1 - y2
+	local result = math.atan2(deltaY, deltaX)
+	
+	if not rad then
+		result = math.deg(result)
+	end
+	
+	return result
+end
+
+-- bezier functions -- <altzeus> to use those, create tables with .x and .y variables and send em like that -- taken from https://github.com/nshafer/Bezier/blob/master/Bezier.lua
+function bezier4(p1,p2,p3,p4,mu)
+	local mum1,mum13,mu3;
+	local p = {}
+
+	mum1 = 1 - mu
+	mum13 = mum1 * mum1 * mum1
+	mu3 = mu * mu * mu
+
+	p.x = mum13*p1.x + 3*mu*mum1*mum1*p2.x + 3*mu*mu*mum1*p3.x + mu3*p4.x
+	p.y = mum13*p1.y + 3*mu*mum1*mum1*p2.y + 3*mu*mu*mum1*p3.y + mu3*p4.y
+
+	return p	
+end
+
+function createCubicCurve(p1, p2, p3, p4, steps) -- <altzeus> steps are required - just do it via distance()
+	points = {}
+	for i = 0, steps do
+		table.insert(points, bezier4(p1, p2, p3, p4, i/steps))
+	end
+	return points
+end
