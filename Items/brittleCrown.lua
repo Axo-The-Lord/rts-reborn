8622610cded542f340d66478f28a1e1d32ce588b
affenstark <22705569+affenstark@users.noreply.github.com>
2022-01-09 14:22:54 +0000

    BrittleCrown: onHit:coins instead of number. onDamage:coin text offset

--- Items/brittleCrown.lua
+++ Items/brittleCrown.lua
@@ -1,46 +1,45 @@
-local EfGold = Object.find("EfGold", "vanilla")
-
 -- Brittle Crown
 local item = Item("Brittle Crown")
 item.pickupText = "Gain gold on hit... BUT lose gold on getting hit."
 item.sprite = Sprite.load("Items/resources/crown.png", 1, 12, 13)
 Lunar.addItem(item)
 item.color = LunarColor
-local c = tostring(LunarColor)
+--local c = tostring(LunarColor)
 
+local EfGold = Object.find("EfGold", "vanilla")
 local sound = Sound.find("Coin", "vanilla")
 
 -- Lose Gold
 callback.register("onDamage", function(hit, damage)
-  if isa(hit, "PlayerInstance") and damage > 0 then
-    local stack = hit:countItem(item)
-    if stack > 0 then
-      local player = hit -- Not this again!
-      local goldLoss = math.ceil(damage * stack)
-      if not net.online or net.localPlayer == player then
-        misc.setGold(math.max(misc.getGold() - goldLoss, 0))
-      end
-      sound:play(0.8 * math.random() * 0.4)
-      if misc.getOption("video.show_damage") then
-        CreateDamageText("-"..math.round(goldLoss), player.x - 12, player.y - 12, Color.ROR_YELLOW)
-      end
-    end
-  end
+	if isa(hit, "PlayerInstance") and damage > 0 then
+		local stack = hit:countItem(item)
+		if stack > 0 then
+			local player = hit -- Not this again!
+			local goldLoss = math.ceil(damage * stack)
+			if not net.online or net.localPlayer == player then
+				misc.setGold(math.max(misc.getGold() - goldLoss, 0))
+			end
+				sound:play(0.8 * math.random() * 0.4)
+			if misc.getOption("video.show_damage") then
+				CreateDamageText("-"..math.round(goldLoss), player.x - 12, player.y - 12, Color.ROR_YELLOW)
+			end
+		end
+	end
 end)
 
 -- Gain Gold
 callback.register("onHit", function(damager, actor, x, y)
-  local parent = damager:getParent()
-  if isa(parent, "PlayerInstance") then
-    local stack = parent:countItem(item)
-    if stack > 0 then
-      local goldGain = 2 * stack * Difficulty.getScaling(cost)
-      if math.chance(30) then
-        local goldInst = EfGold:create(x, y)
-        goldInst:set("value", math.clamp(math.random(goldGain), 2, math.huge))
-      end
-    end
-  end
+	local parent = damager:getParent()
+	if isa(parent, "PlayerInstance") then
+		local stack = parent:countItem(item)
+		if stack > 0 then
+			local goldGain = 2 * stack * Difficulty.getScaling(cost)
+			if math.chance(30) then
+				local goldInst = EfGold:create(x, y)
+				goldInst:set("value", math.clamp(math.random(goldGain), 2, math.huge))
+			end
+		end
+	end
 end)
 
 -- Item Log
@@ -50,10 +49,10 @@ item:setLog{
 	story = "A wretched carnival.\n\nThey were doomed for good reason. Dunepeople of Aphelia: lost, in fanatic worship of parasitic influences. Lemurians: destined to a dead planet, picked clean. Chitin beasts. Automations of death. Why do you bring them home? They were not meant to survive.\n\nI have watched you for ages, from my dead rock - and every century, you disgust me with vanity. You invite vermin into your home. Wretches. Rats. Monsters. Creatures without restraint. Each and every one, planet killers. And yet, you entertain them as guests. Like children, requiring saving and protection.\n\nShe should have died for me. Her gift was wasted on you.\n\nAnd when will we open discussion - dear brother - of all your thin lies? Why do you forbid your guests to leave? To pilot? Why do you fashion great walls and gates? Why do you weave constructs of destruction, if your role is protection? They are entries in your collection. You slaver. Gatekeeper. Hoarder.\n\nYour death is fated. When you die - and you WILL die - I will be ready. I have been patient for millennia. That planet... is mine.",
 	destination = "Some Place", -- Add destination!
 	date = "Some Date", -- Add date!
-	priority = "&"..item.color.."&Unaccounted For&!&"
+	priority = colorString("Unaccounted For", LunarColor)
 }
 
 -- Tab Menu
 if modloader.checkMod("Starstorm") then
-  TabMenu.setItemInfo(item, nil, "30% chance to gain 2 gold on hit, but lose gold when you are hit.", "+2 gold gain, +100% gold loss.")
+	TabMenu.setItemInfo(item, nil, "30% chance to gain 2 gold on hit, but lose gold when you are hit.", "+2 gold gain, +100% gold loss.")
 end
